d5460c4f89c7ff1974b802f2814cfb6878bd3bfc
Lisa Podszun <lisa.podszun@gmx.de>
2024-10-15 16:11:05 +0200

    update classes

--- test_process_model_structure.rb
+++ test_process_model_structure.rb
@@ -4,7 +4,9 @@ require 'securerandom'
 require_relative 'state_machines'
 
 class ServiceCall
-  def initialize(id)
+  attr_reader :id
+  def initialize(id, parent_process)
+     @parent_process = parent_process
      @id = id
      @state_machine =  ServiceCallMachine.new
   end
@@ -15,36 +17,46 @@ class ServiceCall
          state_machine.call
       else
         raise "ERROR: this #{type} event happens in wrong state"
+      end
     when "error"
       if state_machine.can_error?
          state_machine.error
       else
         raise "ERROR: this #{type} event happens in wrong state"
+      end
     when "content"
       if state_machine.can_get_content?
          state_machine.get_content
       else
         raise "ERROR: this #{type} event happens in wrong state"
+      end
     when "receiving"
       if state_machine.can_receive? 
          state_machine.receive
       else
-        raise "ERROR: this #{type} event happens in wrong state"  
+        raise "ERROR: this #{type} event happens in wrong state"
+      end  
     when "done"
       if state_machine.can_end?
          state_machine.end_activity
       else
-        raise "ERROR: this #{type} event happens in wrong state"
+         raise "ERROR: this #{type} event happens in wrong state"
+      end
+      if state_machine.done?
+        @parent_process.signal_end(@id)
+      else
+        raise "ERROR: state machine is not yet done to signal the end"
+      end
     else
       raise "ERROR: Eventtype not possible for #{this.class}"
     end
-  def ended?
-    @state_machine.done?
-  end 
+  end
 end
 
 class ServiceScriptCall
-  def initialize(id)
+  attr_reader :id
+  def initialize(id, parent_process)
+    @parent_process = parent_process
     @id = id
     @state_machine = ServiceScriptCallMachine.new
   end
@@ -55,45 +67,56 @@ class ServiceScriptCall
          state_machine.call
       else
         raise "ERROR: this #{type} event happens in wrong state"
+      end
     when "error"
       if state_machine.can_error?
          state_machine.error
       else
         raise "ERROR: this #{type} event happens in wrong state"
+      end
     when "content"
       if state_machine.can_get_content?
          state_machine.get_content
       else
         raise "ERROR: this #{type} event happens in wrong state"
+      end
     when "receiving"
       if state_machine.can_receive? 
          state_machine.receive
       else
         raise "ERROR: this #{type} event happens in wrong state"
+      end
     when "manipulating"
       if state_machine.can_manipulate?
          state_machine.manipulate
       else 
         raise "ERROR: this #{type} event happens in wrong state"
+      end
     when "change"
       if state_machine.can_change_dataelements?
          state_machine.change_dataelements
       else 
         raise "ERROR: this #{type} event happens in wrong state"  
+      end
     when "done"
       if state_machine.can_end?
          state_machine.end_activity
       else
         raise "ERROR: this #{type} event happens in wrong state"
+      end
+      if state_machine.done?
+        @parent_process.signal_end(@id)
+      else
+        raise "ERROR: state machine is not yet done to signal the end"
+      end
     else
       "ERROR: Eventtype not possible for #{this.class}"
     end
-    def ended?
-      @state_machine.done?
-    end 
+  end
 end
 
 class ScriptCall
+  attr_reader :id
   def initialize(id, parent_process)
     @parent_process = parent_process
     @id = id
@@ -106,51 +129,109 @@ class ScriptCall
          state_machine.manipulate
       else
         raise "ERROR: this #{type} event happens in wrong state"
+      end
     when "error"
       if state_machine.can_error?
          state_machine.error
       else 
         raise "ERROR: this #{type} event happens in wrong state"
+      end
     when "change"
       if state_machine.can_change_dataelements?
          state_machine.change_dataelements
          state_machine.end_activity
       else
         raise "ERROR: this #{type} event happens in wrong state"
+      end
+      if state_machine.done?
+        @parent_process.signal_end(@id)
+      else
+        raise "ERROR: state machine is not yet done to signal the end"
+      end
     else
       "ERROR: Eventtype not possible for #{this.class}"
     end
+  end
+end
+
+def translate_from_xml(node)
+
+  case node.qname.name
+  when "call"
+    with_scripts = false
+    node.children.each do |child_node| 
+      if child_node.qname.name == "code"  
+        with_scripts = true 
+      end
+    end
+    if with_scripts
+      ServiceScriptCall(node.attributes["id"]) 
+    else
+      ServiceCall(node.attributes["id"])
+    end
+  when "manipulate"
+      ScriptCall(node.attributes["id"])
+  when "parallel"
+      ParallelGateway(node)
+  when "parallel_branch"
+      ParallelBranch(node)
+  when "choice"
+    # todo 
+  when "alternative"
+    # todo
+  when "otherwise"
+    # todo
+  else
+    # todo
+  end
 end
 
 
 class Branch
   attr_accessor :subelements
-  @current_element
   @subelements = 0
-  def initialize(process)
-    @parent_process = process
+  def initialize(xml_node, parent_process)
+    @parent_process = parent_process
     @branch_id = SecureRandom.hex(5)
+    @xml_node = xml_node
+    @current_elements = {}
     @structure = []
+    @index = 0
   end
-  def add_element(element)
-    @structure << element
+  
+  def process_log(type, id)
+    if @current_elements.key?(id)
+       @current_elements[id].event(type)
+    else
+      
   end
-  def next_element
-    if subelements == 0
-      @current_element = @structure.shift
-      if @current_element.class == (ParallelGateway || DecisionGateway)
-        @subelements += 1
+
+  def signal_end(id)
+    unless @parent_process.nil?
+      @parent_process.signal_end(id)
+      @current_elements.delete(id)
+    else
+      @current_elements.delete(id)
+      next_elements
+  end
+
+
+  def next_elements
+      @structure << translate_from_xml(xml_node[@index], this)
+      if @structure[@index].class == (ScriptCall || ServiceScriptCall || ServiceCall)
+         @current_elements.store(@structure[@index].id,@structure[@index])
+      else 
+         @current_elements << @structure[@index].next_elements
       end
-    else 
-      @current_element = @current_element.next_element
-    end
+      @index += 1
   end
 end
 
 class ParallelGateway 
-  attr_accessor :wait_on_branches :current_elements
+  attr_accessor :wait_on_branches, :current_elements
 
-  def initialize(wait, cancel)
+  def initialize(xml_node)
+    @executing_branches = []
     @current_elements = []
     @wait = wait
     if @wait == -1
@@ -158,51 +239,68 @@ class ParallelGateway
     else
       @wait_on_branches = wait
       @cancel = cancel
+    end
   end
 
   def add_element(parallel_branch)
     @structure << parallel_branch
     if @wait < 0
       @wait_on_branches += 1
+    end
   end
 
-  def next_element
-    @structure.each do |element|
-      @current_elements << element.next_element
+  def next_element(branch)
+    @current_elements << @structure.at(@structure.index(branch)).next_element
+  end
+
+  def update_current_elements(element)
+    @current_elements.delete(element)
+    next_element
+  end
+
+
+  def cancel_other_branches(branch)
+    if @wait_on_branches - 1 > 0
+      @wait_on_branches -= 1
+      @executing_branches << branch
+    else
+      @executing_branches << branch
+      @structure.each do |branch|
+        if !executing_branches.include? branch.branch_id
+          @structure.delete(branch)
+        end
+      end
     end
   end
 
   def signal_end(branch)
-
-    if @parallel_gateway.wait == -1 && @parallel_gateway.cancel == "after_last"
-       @parallel_gateway.wait_on_branches = @parallel_gateway.wait_on_branches-1
-    elsif @parallel_gateway.wait == -1 && @parallel_gateway.cancel == "after_first"
-      # TODO What is this case?
-    elsif @parallel_gateway.wait != -1 && @parallel_gateway.cancel == "after_last"
-       @parallel_gateway.wait_on_branches = @parallel_gateway.wait_on_branches-1
-    else
-       @parallel_gateway.wait_on_branches = 0
-    end 
+         
   end
-  
 end
 
 class ParallelBranch < Branch
-  attr_reader :parallel_gateway
+  attr_reader :parallel_gateway, :cancel
 
-  def initialize(parallel_gateway, cancel)
+  def initialize(xml_node,parallel_gateway)
     @parallel_gateway = parallel_gateway
     @cancel = cancel
   end
+  def task_done
+    @parallel_gateway.update_current_elements(@current_element,)
+  end
+  def is_first
+    @parallel_gateway.cancel_other_branches(@branch_id)
+  end
 
-  def is_done(this)
-    @parallel_gateway.signal_end_of(this)
+  def is_done
+    @parallel_gateway.signal_end(@branch_id)
   end
 end
 
 
 
-
+class Loop
+end
 
 
 class DecisionGateway
--- test_xml_parsing.rb
+++ test_xml_parsing.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/ruby
 require 'rubygems'
 require "xml/smart"
-#require_relative "test_process_model_structure"
+require_relative "test_process_model_structure"
 
 
+def get_current_nodes(nodes, active_tasks)
+    
+end
 
 
 
@@ -18,36 +21,7 @@ doc.register_namespace 'd', 'http://cpee.org/ns/description/1.0'
 nodes = doc.find("/p:testset/p:description/d:description/d:*")
 active_tasks = []
 
-def get_current_nodes(nodes)
-    node = nodes.first
-    
-    case node.qname.name
 
-    when "call"
-      if node.children.include?("code")
-        active_tasks << ServiceScriptCall.new(node.attributes["id"])
-        nodes.shift
-        nodes
-      else
-        active_tasks << ServiceCall.new(node.attributes["id"])
-        nodes.shift
-        nodes
-    when "manipulate"
-        active_tasks << ScriptCall.new(node.attributes["id"])
-        nodes.shift
-        nodes
-    when "parallel"
-        node.each do |child_node| get_current_nodes(child_node.children) end
-    when "parallel_branch"
-        get_current_nodes(node.children)
+# iterate over model
 
-
-
-    when ""
-    end
-end
-
-
-
-
-#nodes.each {|node| if node.qname.name == "parallel" then p node.children[1].children[0].qname.name end}
+p nodes[0].attributes["id"]
