761b543cac0ccd00cfcd7bd7e112c40187d68527
Lisa Podszun <lisa.podszun@gmx.de>
2024-11-24 19:18:56 +0100

    add config to frontend

--- frontend/page.js
+++ frontend/page.js
@@ -4,17 +4,24 @@ $(document).ready(function(){
     let url = "https://echo.bpm.in.tum.de/fulltest/configuration";
     // let url = "localhost:9303/fulltest/configuration";
     $.getJSON(url, function(data){
-        console.log(data);
-        console.log(data["process_engines"])
         for (let index in data["process_engines"]) {
             let item = data["process_engines"][index]
             console.log(item);
             ($('select[name="process-engine-form"]')).append($(new Option(item["name"], item["url"])));
         };
-
+        for (let index in data['execution_handlers']) {
+            let item = data['execution_handlers'][index];
+            console.log(item);
+            ($('select[name="executionhandler"]')).append($(new Option(item, item)));
+        };
+        for (let index in data['tests']) {
+            let item = data['tests'][index];
+            console.log(item);
+            ($('#test_case')).append($(new Option(item, item)));
+        };
     });
 
-    
+
     
     
 
