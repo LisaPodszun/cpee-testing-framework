40e80ce7b268094f45d7ecccfdad2b0aa61375c5
Lisa Podszun <lisa.podszun@gmx.de>
2024-12-02 16:25:23 +0100

    bug fix

--- frontend/page.js
+++ frontend/page.js
@@ -60,7 +60,7 @@ async function displayResults(data_promise) {
 
                 // put one block [ ins_1_element || matches_ins_1[index_1]]
                 inner_row.append(`<h5 class='headings'>${value['log_instance_1'][index_1]['channel']}</h5>`);
-                let ins_1_log = $('<div class="col border-right border border-primary"></div>').text('Instance 1')
+                let ins_1_log = $('<div class="col border-right border border-primary"></div>').text('Instance 1');
                 let json_1 = JSON.stringify(value['log_instance_1'][index_1]['message'], undefined, 2);
                 ins_1_log.append(json_1);
                 let ins_2_log = $('<div class="col border-left border border-primary"></div>').text('Instance 2'+ matches_ins_1[index_1]);
@@ -84,8 +84,8 @@ async function displayResults(data_promise) {
                 // put one block [matches_ins_2[index_2]  || ins_2_element ]
                 inner_row.append(`<h5 class='headings'>${value['log_instance_1'][index_1]['channel']}</h5>`);
                 let ins_1_log = $('<div class="col border-right border border-primary"></div>').text("Instance 1" + matches_ins_2[index_2]);
-                let ins_2_log = $('<div class="col border-left border border-primary"></div>');
-                let json_2 = text(JSON.stringify((value['log_instance_2'][index_2]['message']), undefined, 2));
+                let ins_2_log = $('<div class="col border-left border border-primary"></div>').text("Instance 2");
+                let json_2 = JSON.stringify((value['log_instance_2'][index_2]['message']), undefined, 2);
                 ins_2_log.append(json_2);
                 inner_row_panel.append(ins_1_log,ins_2_log);
                 inner_col.append(inner_row_panel);
