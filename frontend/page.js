dc401344e94e6b6cec60ce61031636553acef92f
Lisa Podszun <lisa.podszun@gmx.de>
2024-12-09 14:59:47 +0100

    debug

--- frontend/page.js
+++ frontend/page.js
@@ -23,7 +23,7 @@ function markInnerContentResults (log_entry, index, differences_hash) {
             keys = value.split("_");
             element_index = 0;
             for (i = 0; i < keys.length; i++) {
-                console.log("Current element to search for: " + keys[i]);
+                console.log("Current element to search for: " + "\"" + keys[i] + "\"");
                 element_index = log_entry.indexOf("\"" + keys[i] + "\"", element_index);
                 console.log("Current Search Index:" + element_index);
             }
