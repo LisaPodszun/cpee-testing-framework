2ec1093600e49c3aa1fcbab87b08b32bac911405
Lisa Podszun <lisa.podszun@gmx.de>
2024-12-11 12:43:08 +0100

    fix syntax error

--- frontend/page.js
+++ frontend/page.js
@@ -7,7 +7,7 @@ function markInnerStructureResults (log_entry, index, differences_hash) {
         element_index = 0;
         for (i = 0; i < keys.length; i++) {
             element_index = log_entry.indexOf("\"" + keys[i] + "\"", element_index);
-            console.log("Current start index" : element_index);
+            console.log("Current start index" + element_index);
         }
         end_index = log_entry.lastIndexOf("\"" + keys[i] + "\"", element_index);
         console.log("Current End Index:" + end_index);
