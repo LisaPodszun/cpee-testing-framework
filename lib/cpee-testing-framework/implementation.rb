c44f51ae4cb10ae478b4bb95a3d3a8d14c314a67
Lisa Podszun <lisa.podszun@gmx.de>
2025-01-17 22:31:11 +0100

    dbg

--- lib/cpee-testing-framework/implementation.rb
+++ lib/cpee-testing-framework/implementation.rb
@@ -226,6 +226,7 @@ module CPEE
           run Instances, opts[:testinstances] if get
           on resource '\d+' do |res|
             p res[:r].last
+            p opts[:testinstances]
             p opts[:testinstances][res[:r].last]
             run Status, opts[:testinstances][res[:r].last] if get
           end
