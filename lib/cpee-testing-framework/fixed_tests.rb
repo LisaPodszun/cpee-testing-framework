24741b8f11427d2b3f7b83848e29fa144f704f23
Lisa Podszun <lisa.podszun@gmx.de>
2025-01-17 16:02:38 +0100

    dbg

--- lib/cpee-testing-framework/fixed_tests.rb
+++ lib/cpee-testing-framework/fixed_tests.rb
@@ -46,6 +46,8 @@ module TestHelpers
         if testcase != 'custom'
             config['tests'].each do |testgroup|
                 testgroup.each do |entry|
+                    puts entry
+                    puts testcase
                     if entry['name'] == testcase
                         doc_url_ins_1 = entry[settings['instance_1']['execution_handler']]
                         doc_url_ins_2 = entry[settings['instance_2']['execution_handler']]
